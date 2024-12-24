1126ae30c0fabe4aa01ecc02c593a4ab33bbdd98
mavish12 <slodh079@gmail.com>
2024-12-24 15:24:48 +0530

    Single page done

---
+++ scriptapp.js
@@ -0,0 +1,4 @@
+document.getElementById('book-appointment-button').addEventListener('click', function() {
+    var form = document.getElementById('appointment-form');
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+});
